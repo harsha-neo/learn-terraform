eaf4786fa1af813dfdc986b1dd80811a9f5a4478
saiharsha <saiharshavardhan.batta@gmail.com>
2024-08-14 21:53:02 +0100

    terraform changes

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -8,6 +8,14 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "www" {
+  zone_id = "Z0698671ZTK19IKWO19R"
+  name    = "frontend.dev.harshaneo.online"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongo" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.micro"
