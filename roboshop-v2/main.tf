e1d82280d36437392094cad67f30cde2843d132f
saiharsha <saiharshavardhan.batta@gmail.com>
2024-08-16 12:09:39 +0100

    terraform changes

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -1,21 +1,3 @@
-variable "ami" {
-  default = "ami-041e2ea9402c46c32"
-}"
-
-variable "instance_type" {
-  default = "t3.micro"
-}
-
-variable "security_group_ids" {
-    default = ["sg-081c83c9e9e2eb782"]
-}
-
-variable "zone_id"{
-    default = "Z0698671ZTK19IKWO19R"
-    }
-
-
-
 resource "aws_instance" "frontend" {
   ami           = var.ami
   instance_type = var.instance_type
@@ -35,9 +17,9 @@ resource "aws_route53_record" "frontend" {
 }
 
 resource "aws_instance" "mongo" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-081c83c9e9e2eb782"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group_ids
 
   tags = {
     Name = "mongo"
@@ -45,7 +27,7 @@ resource "aws_instance" "mongo" {
 }
 
 resource "aws_route53_record" "mongo" {
-  zone_id = "Z0698671ZTK19IKWO19R"
+  zone_id = var.zone_id
   name    = "mongo.dev.harshaneo.online"
   type    = "A"
   ttl     = 15
@@ -53,9 +35,9 @@ resource "aws_route53_record" "mongo" {
 }
 
 resource "aws_instance" "catalogue" {
-  ami           = "ami-041e2ea9402c46c32"
-  instance_type = "t3.micro"
-  vpc_security_group_ids = ["sg-081c83c9e9e2eb782"]
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group_ids
 
   tags = {
     Name = "catalogue"
@@ -63,7 +45,7 @@ resource "aws_instance" "catalogue" {
 }
 
 resource "aws_route53_record" "catalogue" {
-  zone_id = "Z0698671ZTK19IKWO19R"
+  zone_id = var.zone_id
   name    = "catalogue.dev.harshaneo.online"
   type    = "A"
   ttl     = 15
---
+++ roboshop-v2/variables.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+  default = "ami-041e2ea9402c46c32"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "security_group_ids" {
+    default = ["sg-081c83c9e9e2eb782"]
+}
+
+variable "zone_id"{
+    default = "Z0698671ZTK19IKWO19R"
+}
+
