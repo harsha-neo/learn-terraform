c9f8b873700559cc66434dc26ea02d729e40ba04
saiharsha <saiharshavardhan.batta@gmail.com>
2024-08-20 21:56:57 +0100

    terraform changes

--- roboshop-v2/variables.tf
+++ roboshop-v2/variables.tf
@@ -7,9 +7,6 @@ variable "domain_name" {
   default = "harshaneo.online"
 }
 
-variable "zone_id" {
-  default = harshaneo.online
-}
 
 variable "components" {
   default = ["frontend", "catalogue", "mongo"]
