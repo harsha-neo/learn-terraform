5dfdb3f4aa58c5c83501e701b6fb9d82351699a9
saiharsha <saiharshavardhan.batta@gmail.com>
2024-08-20 21:12:20 +0100

    terraform changes

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -9,12 +9,13 @@ resource "aws_instance" "instance" {
   }
 }
 
-#resource "aws_route53_record" "dns_record" {
- # zone_id = data.aws_route53_zone.zone.zone_id
-  #name    = "frontend.dev.${var.domain_name}"
-  #type    = "A"
-  #ttl     = 15
-  #records = [aws_instance.frontend.private_ip]
-#}
+resource "aws_route53_record" "dns_record" {
+  count = length(var.components)
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "${var.components[count.index]}frontend.dev.${var.domain_name}"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.instance[count.index].private_ip]
+}
 
 
